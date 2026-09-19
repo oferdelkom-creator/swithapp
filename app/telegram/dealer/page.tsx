95f542fd57f2a358d36f703a2a6f138cd19ce8c2
oferdelkom-creator <ofer.delkom@gmail.com>
2026-09-19 22:50:04 +0300

    Add Telegram dealer page

---
+++ app/telegram/dealer/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+import TelegramDealerPortal from "./TelegramDealerPortal";
+
+export const metadata: Metadata = {
+  title: "SwitchApp для дилеров и импортёров",
+  description: "Управление автомобилями и заявками в Telegram.",
+};
+
+export default function TelegramDealerPage() {
+  return <TelegramDealerPortal />;
+}
+
+
